dec6d5f67370a74fd296e973f45ce949b5930c4c
jlox <altermuse3@gmail.com>
2016-03-06 15:39:33 -0500

    user input

--- pong.js
+++ pong.js
@@ -16,100 +16,139 @@ var move_right = false;
 
 //clears the screen
 var blank = function(e) {
-  ctx.beginPath();
-  ctx.fillStyle = "#333333";
-  ctx.fillRect(0,0,500,500);
+    ctx.beginPath();
+    ctx.fillStyle = "#333333";
+    ctx.fillRect(0,0,500,500);
 }
 //puts the ball at the ball's x & y coordinates
 var ball = function(e){
-  ctx.beginPath();
-  ctx.fillStyle = "rgb(150,255,150)";
-  ctx.strokeStyle = "black";
-  ctx.arc(ball_x, ball_y, 15, 0, 2*Math.PI);
-  ctx.fill();
-  ctx.stroke();
+    ctx.beginPath();
+    ctx.fillStyle = "rgb(150,255,150)";
+    ctx.strokeStyle = "black";
+    ctx.arc(ball_x, ball_y, 15, 0, 2*Math.PI);
+    ctx.fill();
+    ctx.stroke();
 }
 
 //makes corners for the bar
 var make_ball = function (a,b) {
-  ctx.beginPath();
-  ctx.arc(a, b, 3, 0, 2*Math.PI);
-  ctx.fill();
+    ctx.beginPath();
+    ctx.arc(a, b, 3, 0, 2*Math.PI);
+    ctx.fill();
 }
 
 //builds a bar whose top middle is at (a,b)
 var bar = function(a,b){
-  ctx.beginPath();
-  ctx.fillStyle = "rgb(130,130,130)";
-  ctx.strokeStyle = "black";
-  ctx.fillRect(a-45, b, 90, 25);
-  ctx.fillRect(a-50, b+5, 100, 15);
-  make_ball(a-47,b+3);
-  make_ball(a+47,b+3);
-  make_ball(a-47,b+22);
-  make_ball(a+47,b+22);
-  ctx.fill();
-}
-//puts the bars at the locations indicated by their x coordinates
-var bars = function(e){
-  bar(control_x, 470);
-  bar(ai_x, 5);
+    ctx.beginPath();
+    ctx.fillStyle = "rgb(130,130,130)";
+    ctx.strokeStyle = "black";
+    ctx.fillRect(a-45, b, 90, 25);
+    ctx.fillRect(a-50, b+5, 100, 15);
+    make_ball(a-47,b+3);
+    make_ball(a+47,b+3);
+    make_ball(a-47,b+22);
+    make_ball(a+47,b+22);
+    ctx.fill();
 }
 
-var setup = function(e){
-  blank();
-  ball();
-  bars();
+//puts the bars at the locations indicated by their x coordinates
+var bars = function(e){
+    bar(control_x, 470);
+    bar(ai_x, 5);
 }
+
+//draws everything
+var setup = function(e){
+    blank();
+    ball();
+    bars();
+}
+
 var reset = function() {
-  game_on = false;
-  ball_x = 250;
-  ball_y = 455;
-  x_dir = 1;
-  y_dir = -1;
-  control_x = 250;
-  ai_x = 250;
-  losses++;
-  move_left = false;
-  move_right = false;
+    game_on = false;
+    ball_x = 250;
+    ball_y = 455;
+    x_dir = 1;
+    y_dir = -1;
+    control_x = 250;
+    ai_x = 250;
+    losses++;
+    move_left = false;
+    move_right = false;
 }
 
 //how the ball moves
 var move_commands = function(){
-  ball_x+=x_dir;
-  ball_y+=y_dir;
-  if(ball_x <= 5 || ball_x >= 495){
-    x_dir=x_dir*-1;
-  }
-  if(ball_y >= 470 /*&& Math.abs(control_x - ball_x)<=50*/){
-    y_dir=-1;
-  }
-  if(ball_y <= 45){
-    y_dir = 1;
-  }
-  if(move_right && control_x < 450){
-    control_x++;
-  }
-  if(move_left && control_x > 50){
-    control_x--;
-  }
-  if(ball_y >= 495){
-    reset();
-  }
-  if(ball_y < 300)
-    if(ball_x - ai_x > 20 && ai_x < 450){
-      ai_x+=1.2;
+    ball_x+=x_dir;
+    ball_y+=y_dir;
+    if(ball_x <= 5 || ball_x >= 495){
+	x_dir=x_dir*-1;
     }
+    if(ball_y >= 470 /*&& Math.abs(control_x - ball_x)<=50*/){
+	y_dir=-1;
+    }
+    if(ball_y <= 45){
+	y_dir = 1;
+    }
+    if(move_right && control_x < 450){
+	control_x++;
+    }
+    if(move_left && control_x > 50){
+	control_x--;
+    }
+    if(ball_y >= 495){
+	reset();
+    }
+    if(ball_y < 300)
+	if(ball_x - ai_x > 20 && ai_x < 450){
+	    ai_x+=1.2;
+	}
     if(ball_x - ai_x < 20 && ai_x > 50){
-      ai_x-=1.2;
+	ai_x-=1.2;
+    }
+    if(move_right && (control_x<450)){
+	control_x+=1.5;
+	moveBar();
+	//move_left = false;
+    }
+    if(move_left && (control_x>50)){
+	control_x-=1.5;
+	moveBar();
+	//move_right = false;
+    }
+    if ((ball_y == 460) && ((ball_x > (control_x - 50)) && (ball_x < (control_x + 50)))){
+	y_dir = -y_dir;
+    }
+    //100x25 - if ball goes beyond "control_y", and isnt in range of the bar, reset
+    if((ball_y>460) && ((ball_x < (control_x - 50)) || (ball_x > (control_x + 50)))){
+	reset();
+    }
+    
+}
+
+//user input
+var moveBar = function(e){
+    if (e.keyCode == 37){
+	move_left=true;
+    } else if (e.keyCode == 39){
+	move_right=true;
     }
 }
+
+var stopBar = function(e){
+    move_left=false;
+    move_right=false;
+}
+
 //commands that run every frame
 var frame = function(e){
     setup();
     console.log('losses:'+losses);
     if(game_on){
-      move_commands();
+	move_commands();
     }
 }
 var go = setInterval(frame,speed);
+
+window.addEventListener('keydown', moveBar);
+window.addEventListener('keyup', stopBar);
